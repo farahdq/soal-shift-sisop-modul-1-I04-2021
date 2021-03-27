4f67b2502142d3cbae4fb15043f0e2947362e66d
farahdq <73812417+farahdq@users.noreply.github.com>
2021-03-27 19:42:54 +0700

    Create soal2_generate_laporan_ihir_shisop.sh

---
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -0,0 +1,46 @@
+// 2A
+profit = $21/($18 - $21)*100;
+if (profit >= max){max = profit;rowid =  $1;ordid =$2}
+
+//2B 
+	if ($10 == "Albuquerque")
+	x[$7]++
+}
+		for (y in x)
+		{
+		print y}
+    
+ //2C
+ {
+	if (NR!=1)x[$8]++
+	}
+  {
+	min =x["Consumer"];
+	tag = "Customer"
+	for (y in x)
+		if (x[y] < min){
+			min = x[y];
+			tag = y;
+	}
+  
+  //2D
+  {
+	if (NR!=1)x[$13]+=$21
+	} 
+	{
+	min = x["South"];
+	tag = "South"
+	for (y in x)
+		if (x[y] < min){
+			min = x[y];
+			tag = y;
+	}
+
+
+
+
+
+
+
+
+
